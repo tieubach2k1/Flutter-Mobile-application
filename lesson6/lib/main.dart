7a371e915a18e0d7978a2a3e2495bb9425cf20d1
tieubach2k1 <tieubachzzz2k1@gmail.com>
2022-03-04 12:57:46 +0700

    Lesson6

---
+++ lesson6/lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Hello World Demo Application',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Home page'),
+    );
+  }
+}
+/*
+class MyHomePage extends StatelessWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar( title: Text(this.title), ),
+      body: Center( child: Image.asset("assets/Image1.jpg")),
+    );
+  }
+}
+*/
+class MyHomePage extends StatelessWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(this.title),),
+      body: Center( child: Icon(Icons.email)),
+    );
+  }
+}
+
+
