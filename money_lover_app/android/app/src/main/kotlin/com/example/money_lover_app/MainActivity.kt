c19c0ea5018bdcba242a24ec451f6ab0a7e47c4d
tieubach2k1 <tieubachzzz2k1@gmail.com>
2022-03-25 15:16:48 +0700

    Update

---
+++ money_lover_app/android/app/src/main/kotlin/com/example/money_lover_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.money_lover_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ money_lover_app/lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'M♥ney App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(title: '',),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  HomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar( title: Text(this.title), ),
+      body: Center( child: Image.asset("assets/logo.png")),
+    );
+  }
+}
